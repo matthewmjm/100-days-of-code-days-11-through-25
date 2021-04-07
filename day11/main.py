299b494a6d68ec29ee14e234a819866afefd5dbe
matthewmjm <matthewmjm@yahoo.com>
2021-04-07 15:46:28 -0600

    initial setup for day 11

--- day11/main.py
+++ day11/main.py
@@ -1,1 +1,13 @@
-# Capstone Project from Days 1 - 10
+print("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+print("Your cards: [2, 8], current score: 10")
+print("Computer's first card: ")
+print("Type 'y' to get another card, type 'n' to pass: ")
+
+print("Your cards: [6, 10, 10], current score: 26")
+print("Your final hand: [2, 8, 6, 10], final score: 26")
+print("Your final hand: [8, 10], final score 18")
+print("Computer's final hand: [8, 10], final score 18")
+print("You went over.  You lose 😭")
+print("You lose 😤")
+print("You win 😃")
---
+++ day11/prints.py
@@ -0,0 +1,36 @@
+print("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+print(" Your cards: [2, 8], current score: 10")
+print(" Computer's first card: 8")
+print("Type 'y' to get another card, type 'n' to pass: ")
+
+# went over and lost before computer went
+print(" Your cards: [6, 10, 10], current score: 26")
+print(" Computer's first card: 8")
+print("Type 'y' to get another card, type 'n' to pass: ")
+print(" Your final hand: [2, 8, 6, 10], final score: 26")
+print(" Computer's final hand: [8, 10], final score 18")
+print("You went over.  You lose 😤")
+print(" Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+# hit, held, computer went over
+print(" Your cards: [6, 10], current score: 16")
+print(" Computer's first card: 10")
+print("Type 'y' to get another card, type 'n' to pass: ")
+print(" Your cards: [6, 10, 4], current score: 20")
+print(" Computer's first card: 10")
+print("Type 'y' to get another card, type 'n' to pass: ")
+print(" Your final hand: [6, 10, 4], final score: 20")
+print(" Computer's final hand: [10, 8], final score 18")
+print("You win 😃")
+print(" Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+
+print("You went over.  You lose 😤")
+print(" Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+print("You went over.  You lose 😭")
+print("You lose 😤")
+print("You win 😃")
+
+print("Your final hand: [8, 10], final score 18")
