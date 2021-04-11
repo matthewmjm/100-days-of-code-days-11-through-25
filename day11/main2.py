23cbfb1d83941ebbc92c908a8dfea8c4593e7239
matthewmjm <matthewmjm@yahoo.com>
2021-04-11 16:18:12 -0600

    start refactor of BlackJack

--- day11/main2.py
+++ day11/main2.py
@@ -1,17 +1,31 @@
 import random
-def deal_cards():
+
+def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    random.choice(cards)
+    card = random.choice(cards)
     return card
-    user_cards = []
-    computer_cards = []
 
-    for _ in range(2):
-        user_cards.append(deal_card())
-        computer_cards.append(deal_card())
-
-def calculate_scores(cards):
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
     return sum(cards)
 
+user_cards = []
+computer_cards = []
+is_game_over = False
 
-print(cards)
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+print(f"  Your cards: {user_cards}. current_score {user_score}")
+print(f"  Computer's cards: {computer_cards}. current_score {computer_score}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
