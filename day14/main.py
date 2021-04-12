cdc4689f9afe58789c6e4bf9976aafacc5d41594
matthewmjm <matthewmjm@yahoo.com>
2021-04-12 17:21:52 -0600

    Proforma day 14

--- day14/main.py
+++ day14/main.py
@@ -3,5 +3,4 @@ print("Compare A: ")
 print("Against B: ")
 input("Who has more followers? Type 'A' or 'B': ")
 print("You are right!  Current score: 1.")
-print("Sorry, that's wrong.  Final Score: 10
-")
+print("Sorry, that's wrong.  Final Score: 2")
