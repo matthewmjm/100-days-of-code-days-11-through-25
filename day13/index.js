bf0577894828438b4d6322ecccb73b915ade74ca
matthewmjm <matthewmjm@yahoo.com>
2021-04-12 19:57:29 -0600

    finished js drumkit

--- day13/index.js
+++ day13/index.js
@@ -1,8 +1,17 @@
-window.addEventListener("keydown", function (event) {
+function playSound(event) {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
   if (!audio) return
   audio.currentTime = 0
   audio.play()
-  console.log(event.keyCode)
-  console.log(audio)
-})
+  key.classList.add("playing")
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== "transform") return
+  this.classList.remove("playing")
+}
+
+const keys = Array.from(document.querySelectorAll(".key"))
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition))
+window.addEventListener("keydown", playSound)
