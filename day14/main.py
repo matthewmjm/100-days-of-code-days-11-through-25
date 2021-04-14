4d1c88182263995c36d387ced2c1638f860b52f3
matthewmjm <matthewmjm@yahoo.com>
2021-04-14 13:09:55 -0600

    start Day 14 project

--- day14/main.py
+++ day14/main.py
@@ -9,3 +9,12 @@ import random
 from art import logo
 from os import system, name
 from time import sleep
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+clear()
+print(logo)
