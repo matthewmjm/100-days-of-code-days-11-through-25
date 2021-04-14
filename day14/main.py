4b03ac234e097bd1b1395d7e3b25f3b8c62b31de
matthewmjm <matthewmjm@yahoo.com>
2021-04-13 18:09:39 -0600

    start Day 14 project

--- day14/main.py
+++ day14/main.py
@@ -1,6 +1,11 @@
-# https://replit.com/@appbrewery/higher-lower-final?embed=1&output=1#main.py
-print("Compare A: ")
-print("Against B: ")
-input("Who has more followers? Type 'A' or 'B': ")
-print("You are right!  Current score: 1.")
-print("Sorry, that's wrong.  Final Score: 2")
+# # https://replit.com/@appbrewery/higher-lower-final?embed=1&output=1#main.py
+# print("Compare A: ")
+# print("Against B: ")
+# input("Who has more followers? Type 'A' or 'B': ")
+# print("You are right!  Current score: 1.")
+# print("Sorry, that's wrong.  Final Score: 2")
+
+import random
+from art import logo
+from os import system, name
+from time import sleep
